6d5f29c555b48273ce87e9b1701c1b7aa9b7c3a5
Rijesh Sapkota <146547828+rijeshsapkota7@users.noreply.github.com>
2025-07-04 17:50:57 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+  var typed = new Typed("#typed-text", {
+    strings: ["Web Developer", "Software Developer", "Freelancer", "Designer"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+  });
